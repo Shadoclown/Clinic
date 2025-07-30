2e5d0da36d49f6832cfc0199337ab8ce8a406e61
Shadoclown <dungky555@gmail.com>
2025-07-30 12:05:12 +0700

    make a new variable file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Sidebar from './components/Sidebar'  // Fixed capitalization
+import './styles/variable.css'  // Import variables first
+import './App.css'
+import './components/style/Table.css'
+import Sidebar from './components/Sidebar'
 import Home from './components/Home'
 import Appointment from './components/Appointment/Appointment'
 import Examination from './components/Examination'
 import Records from './components/Records'
-import './components/style/Table.css'
-import './App.css'
 
 function App() {
     return (
